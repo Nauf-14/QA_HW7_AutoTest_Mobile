80f6f1086f253e1f2c60d87c7fd331f9d38ba992
Naufal <nauf1207@mail.ru>
2024-05-04 23:58:14 +0600

    Повторите сценарий текущего модуля, заменив нативный поиск элементов поиском с помощью Webview и наоборот.

---
+++ src/main/java/DriverFactory.java
@@ -0,0 +1,26 @@
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class DriverFactory {
+
+    private AndroidDriver driver;
+
+    public AndroidDriver setUp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");;
+        desiredCapabilities.setCapability("appium:automationName", "UIAutomator2");
+
+
+        URL remoteUrl = new URL("http://127.0.0.1:4723");
+
+        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        return driver;
+
+//        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+}
